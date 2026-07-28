c148000c34a51ac31ddbcca57e5d468e78c52a4f
Lars Herschke <lhersch@dssgmbh.de>
2026-07-28 19:04:13 +0200

    Force Java 8

--- .github/workflows/windows_build.sh
+++ .github/workflows/windows_build.sh
@@ -5,7 +5,7 @@ export ICEDTEAWEB_INSTALL="$(cygpath -u "${WORKSPACE}/icedtea-web-image")"
 export WIXPATH="$(cygpath -u "C:/PROGRA~2/WIXTOO~1.14/bin")"
 export WIXGEN="$(cygpath -u "C:/cygwin64/usr/share/java/wixgen.jar")"
 export PATH="${PATH}:/cygdrive/c/rust/bin:${WIXPATH}"
-export JVM_HOME_SHORT="$(cygpath -d "${JAVA_HOME}")"
+export JVM_HOME_SHORT="$(cygpath -d "${JAVA_HOME_8_X64}")"
 export JVMPATH="$(cygpath -u ${JVM_HOME_SHORT})"
 echo "Configure IcedTea-Web"
 ./autogen.sh
