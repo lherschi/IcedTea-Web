826b0a4b00a0cf72ba36e75aa45a1905e8077454
Lars Herschke <lhersch@dssgmbh.de>
2026-07-28 15:35:12 +0200

    Fix for building for Windows with Rust 1.87+

--- launchers/utils.sh
+++ launchers/utils.sh
@@ -37,7 +37,10 @@ function build() {
         cargo test  --no-run
         rm -fv $BUILD_DIR/target/debug/launcher-*.d ;
         $KCOV $BUILD_DIR $BUILD_DIR/target/debug/launcher-*
-      else
+      else    
+        if isWindows; then
+          export RUSTFLAGS="-C link-arg=-ladvapi32"
+        fi
         #on linux patch  out deps?
         RUST_BACKTRACE=1 cargo test
         #some windows?
